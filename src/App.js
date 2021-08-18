b75b54bb618b8532dc3d253c7c4814bb03475766
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-08-18 20:58:14 +0600

    basic navbar and footer done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+import Footer from './Components/Reuse/Footer/Footer';
+import Navbar from './Components/Reuse/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Contact from './Components/Homepage/Contact/Contact';
+import About from './Components/About/About';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Services from './Components/Homepage/Services/Services';
+import Homepage from './Components/Homepage/Homepage/Homepage';
 
 function App() {
     return (
-        <div>
-            <h3 className="text-red-500">Perfect Click</h3>
-        </div>
+        <>
+            <Navbar />
+            <Switch>
+                <Route exact path="/">
+                    <Homepage />
+                </Route>
+                <Route path="/service">
+                    <Services />
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard />
+                </Route>
+                <Route path="/contact">
+                    <Contact />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+            </Switch>
+            <Footer />
+        </>
     );
 }
 
---
+++ src/Components/Reuse/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="flex justify-center items-center h-16 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 text-white">
+            <p>Copyright &copy; 2021 Perfect Click all rights reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
